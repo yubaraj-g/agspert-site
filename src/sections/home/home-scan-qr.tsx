654c6a5e055692efcca945108844fa4b73ebe9f5
yubaraj-g <yuvrajwebdev@gmail.com>
2024-05-10 13:40:25 +0530

    fix: image responsive for mobile

--- src/sections/home/home-scan-qr.tsx
+++ src/sections/home/home-scan-qr.tsx
@@ -6,10 +6,14 @@ import { motion } from "framer-motion";
 import { TypographyH1 } from "@/components/typography";
 import Template from "@/layouts/template";
 
+import useScreenSize from "@/hooks/use-screen-size";
+
 export default function HomeScanQR() {
+  const isDesktop = useScreenSize();
+  
   return (
     <Template>
-      <div className="w-full flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-24 min-h-96 lg:h-[600px] pt-12">
+      <div className="w-full flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-12 lg:gap-24 min-h-96 lg:h-[600px] pt-12">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -23,8 +27,8 @@ export default function HomeScanQR() {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ ease: "easeInOut", duration: 0.75, delay: 0.3 }}
         >
-          <div className="flex grow rounded-tl-2xl rounded-br-2xl overflow-hidden shadow-2xl w-[600px] h-[350px]">
-            <Image src="/images/scan-qr.jpeg" alt="scan qr" width={600} height={500} objectFit="cover" />
+          <div className="flex grow rounded-tl-2xl rounded-br-2xl overflow-hidden shadow-2xl w-[250px] h-[175px] lg:w-[600px] lg:h-[350px]">
+            {isDesktop ? <Image src="/images/scan-qr.jpeg" alt="scan qr" width={600} height={350} /> : <Image src="/images/scan-qr.jpeg" alt="scan qr" width={250} height={175} />}
           </div>
         </motion.div>
       </div>
