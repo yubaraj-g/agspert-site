17c32dc43cf64dda12f230e7505bbee2a00dabfb
yubaraj-g <yuvrajwebdev@gmail.com>
2024-05-19 15:21:27 +0530

    fix: responsive for footer mobile.

--- src/layouts/main/footer.tsx
+++ src/layouts/main/footer.tsx
@@ -22,27 +22,27 @@ export default function Footer() {
   return (
     <div className="container min-h-56 my-12 text-center pt-8 h-full">
       <Template className="h-full">
-        <div className="flex lg:flex-row flex-col items-start justify-between gap-6 h-full">
-          <div className="flex flex-col items-center justify-between lg:items-start text-center lg:text-start gap-3 h-full min-h-56">
-            <div className="flex flex-col gap-3">
+        <div className="flex lg:flex-row flex-col items-center lg:items-start justify-between gap-6 h-full">
+          <div className="flex flex-col items-center justify-between lg:items-start text-center lg:text-start gap-3 h-full lg:min-h-56">
+            <div className="flex flex-col gap-3 items-center lg:items-start">
               <Image src="/images/agspert-logo-nobg.png" alt="agspert" width="200" height="120" />
               <span># 5, Banphool Path, Hatigaon, Guwahati, Assam 781006</span>
             </div>
 
-            <TypographyMuted>
+            <TypographyMuted className="hidden lg:block">
               Copyright ©2020-24 AgSpert - All Rights Reserved.
             </TypographyMuted>
           </div>
 
-          <div className="flex flex-col gap-3">
-            <TypographyH3 className="text-start pl-4">Explore</TypographyH3>
+          <div className="flex flex-col gap-3 items-center lg:items-start">
+            <TypographyH3 className="text-start pl-0 lg:pl-4">Explore</TypographyH3>
 
             <ul className="flex flex-col gap-3 items-start w-full">
               <li className="w-full">
                 <Link href={"/"} className="w-full">
                   <Button
                     variant="ghost"
-                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-start"
+                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-center lg:justify-start"
                   >
                     <TypographySmall>Home</TypographySmall>
                   </Button>
@@ -52,7 +52,7 @@ export default function Footer() {
                 <Link href={"/value-chain"} className="w-full">
                   <Button
                     variant="ghost"
-                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-start"
+                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-center lg:justify-start"
                   >
                     <TypographySmall>Value Chain</TypographySmall>
                   </Button>
@@ -62,7 +62,7 @@ export default function Footer() {
                 <Link href={"/privacy-policy"} className="w-full">
                   <Button
                     variant="ghost"
-                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-start"
+                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-center lg:justify-start"
                   >
                     <TypographySmall>Privacy Policy</TypographySmall>
                   </Button>
@@ -72,7 +72,7 @@ export default function Footer() {
                 <Link href={"/terms-of-service"} className="w-full">
                   <Button
                     variant="ghost"
-                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-start"
+                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-center lg:justify-start"
                   >
                     <TypographySmall>Terms of Service</TypographySmall>
                   </Button>
@@ -81,8 +81,8 @@ export default function Footer() {
             </ul>
           </div>
 
-          <div className="flex flex-col gap-3">
-            <TypographyH3 className="text-start pl-4">Our Platform</TypographyH3>
+          <div className="flex flex-col gap-3 items-center lg:items-start">
+            <TypographyH3 className="text-start pl-0 lg:pl-4">Our Platform</TypographyH3>
 
             <ul className="flex flex-col gap-3 items-start w-full">
               <li className="w-full">
@@ -92,7 +92,7 @@ export default function Footer() {
                 >
                   <Button
                     variant="ghost"
-                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-start"
+                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-center lg:justify-start"
                   >
                     <TypographySmall>Mobile App</TypographySmall>
                   </Button>
@@ -102,7 +102,7 @@ export default function Footer() {
                 <Link href={"https://www.agspeak.in/"} className="w-full">
                   <Button
                     variant="ghost"
-                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-start"
+                    className="w-full border-l-2 border-b-2 border-transparent hover:border-green-250 hover:shadow-xl hover:border-b-2 hover:border-l-2 rounded-none rounded-tl-xl rounded-br-xl flex justify-center lg:justify-start"
                   >
                     <TypographySmall>Web App</TypographySmall>
                   </Button>
@@ -144,6 +144,10 @@ export default function Footer() {
               </Link>
             </div>
           </div>
+
+          <TypographyMuted className="block lg:hidden">
+            Copyright ©2020-24 AgSpert - All Rights Reserved.
+          </TypographyMuted>
         </div>
       </Template>
     </div>
