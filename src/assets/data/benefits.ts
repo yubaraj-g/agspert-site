5c789e4b78469cd69caef3467bd7f95e19dc145e
yubaraj-g <yuvrajwebdev@gmail.com>
2024-05-20 22:06:06 +0530

    change; images for benefits section changed.

--- src/assets/data/benefits.ts
+++ src/assets/data/benefits.ts
@@ -9,37 +9,37 @@ export const BENEFITS = [
     heading: "Boost Efficiency and Waste Reduction",
     details:
       "Minimise food spoilage and operational inefficiencies. Our Blockchain and AI-powered tools streamline processes, saving you time and resources.",
-    image: "/images/boost-efficiency.jpg",
+    image: "/images/boost-efficiency.jpeg",
   },
   {
     heading: "Build Trust and Loyalty",
     details:
       "Showcase your commitment to quality and transparency. Earn trust from consumers by demonstrating the integrity of your products and sourcing practices.",
-    image: "/images/build-trust.jpg",
+    image: "/images/build-trust-loyalty.jpeg",
   },
   {
     heading: "Empower Farmers and Foster Collaboration",
     details:
       "Connect with small farmers in their local languages, empowering them with knowledge and fostering collaboration. Bridge the gap between farmers, businesses, and consumers for a sustainable agricultural ecosystem.",
-    image: "/images/empower-farmers.jpg",
+    image: "/images/empower-farmers.jpeg",
   },
   {
     heading: "Customer Insights",
     details:
       "Increase sales and customer satisfaction by leveraging AgSpeak's insights into customer behaviour and preferences to optimise your marketing strategies effectively.",
-    image: "/images/customer-insights.jpg",
+    image: "/images/customer-insights.jpeg",
   },
   {
     // heading: "Cost Savings",
     heading: "Reduce Downtime With Predictive Patterns",
     details:
       "Save significantly on expenses with AgSpeak's low-cost and intuitive tools. Compared to traditional platforms and tools, our solution ensures maximum ROI for your business operations.",
-    image: "/images/predictive-patterns.png",
+    image: "/images/predictive-patterns.jpeg",
   },
   {
     heading: "Industry Leadership",
     details:
       "Stand out as an industry leader with AgSpeak's pioneering vernacular support in Enterprise SAAS. Enjoy a 30% boost in engagement and customer experience, setting new standards in customer satisfaction and retention.",
-    image: "/images/industry-leadership.jpg",
+    image: "/images/industry-leadership.jpeg",
   },
 ];
